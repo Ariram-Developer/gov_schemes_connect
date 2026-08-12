c5acb6546835170a5f8a93c7d107e406f0f976ff
Ariram-Yadhav <ariramyadhav@gmail.com>
2026-08-12 23:02:02 +0530

    Update config to support both local and Railway database variables

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback_dev_key'
     
     # MySQL Database Config
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-    DB_NAME = os.environ.get('DB_NAME', 'gov_scheme_connect')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_NAME = os.environ.get('DB_NAME')
