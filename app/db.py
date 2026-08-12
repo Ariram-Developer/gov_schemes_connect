7cda789e74c55fd6dfdfe9fca6611d0922f967fa
Ariram-Yadhav <ariramyadhav@gmail.com>
2026-08-12 23:17:59 +0530

    db.py fixed

--- app/db.py
+++ app/db.py
@@ -4,16 +4,16 @@ from mysql.connector import pooling
 from flask import current_app, g
 
 def get_pool():
-    """Initializes the MySQL connection pool."""
+    """Initializes the MySQL connection pool using Flask app config."""
     if 'db_pool' not in current_app.config:
         current_app.config['db_pool'] = mysql.connector.pooling.MySQLConnectionPool(
             pool_name="gov_pool",
             pool_size=5,
             pool_reset_session=True,
-            host=os.environ.get('DB_HOST', 'localhost'),
-            user=os.environ.get('DB_USER', 'root'),
-            password=os.environ.get('DB_PASSWORD', ''),
-            database=os.environ.get('DB_NAME', 'gov_scheme_connect')
+            host=current_app.config['DB_HOST'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD'],
+            database=current_app.config['DB_NAME']
         )
     return current_app.config['db_pool']
 
